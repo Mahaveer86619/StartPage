a27a14cb8b80243c3ecc9e8b54eb63efaa341f7a
MahaveerPanigrahi86619 <mahaveerprasad.panigrahi@synlabs.io>
2025-08-18 15:18:41 +0530

    search improvements

--- js/search.js
+++ js/search.js
@@ -1,5 +1,17 @@
 const searchBar = document.getElementById('search-bar-input');
 
+window.addEventListener('keydown', (event) => {
+    // If the user is typing in an input field, do nothing
+    if (event.target.tagName.toLowerCase() === 'input') {
+        return;
+    }
+
+    // If the pressed key is a character, focus the search bar
+    if (event.key.length === 1) {
+        searchBar.focus();
+    }
+});
+
 searchBar.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         const query = searchBar.value;
